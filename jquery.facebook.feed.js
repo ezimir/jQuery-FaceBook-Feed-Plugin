a88ac68204312f9e9b8eb5f1637edbd7b1d313a6
Martin Tóth <ezimir@gmail.com>
2012-04-02 12:45:31 +0200

    First version of FB Feed loader
    This needs jQuery template plugin (currently hosted on Microsoft CDN).
    There are options that need to be provided always (no checking yet):
    - id: id of user to get feed from
    - page: slug of the page that user icon should link to
    - token: access token
    - template: jQuery selector pointing to template used to display
      results

---
+++ jquery.facebook.feed.js
@@ -0,0 +1,64 @@
+
+(function ($) {
+
+	var fbfeed = 'fbfeed';
+		settings = {},
+		defaults = {
+			items: 1
+		},
+		API_BASE = 'https://graph.facebook.com/',
+		FB_PAGE_BASE = 'https://www.facebook.com/pages/';
+
+	function showError(message) {
+		console.log(message);
+		alert(message);
+	}
+
+	function fbResponse(response) {
+		if (response.error) {
+			return showError(response.error.message);
+		}
+
+		var items = [];
+		for (var i = 0; post = response.data[i]; i++) {
+			var date = /([\d-]+)T([\d:]+)\+(.+)/.exec(post.created_time),
+				item = $.extend({}, post, {
+					date: date[1],
+					time: date[2]
+				});
+
+			items.push(item);
+		}
+
+		var user = items[0].from;
+		user.page_link = FB_PAGE_BASE + settings.page + '/' + user.id;
+
+		showItems(user, items);
+	}
+
+	function showItems(user, items) {
+		var data = {
+				API_BASE: API_BASE,
+				user: user,
+				items: items
+			};
+
+		console.log(items);
+
+		$(settings.template).tmpl(data).appendTo(settings.$target);
+	}
+
+	$.fn[fbfeed] = function (options) {
+		settings = $.extend({}, defaults, options);
+		settings.$target = this;
+
+		settings.$target.empty();
+
+		$.ajax({
+			url: API_BASE + settings.id + '/feed/?limit=' + settings.items + '&access_token=' + settings.token,
+			dataType: 'jsonp',
+			success: fbResponse
+		});
+	}
+
+}(jQuery));
